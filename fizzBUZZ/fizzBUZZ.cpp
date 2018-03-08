9ce20edf44e94b6b9aa8876203400ebbfcf1bddc
Justin Burt <jstnbrt@hotmail.com>
2018-03-07 23:15:25 -0600

    Initial commit

---
+++ fizzBUZZ/fizzBUZZ.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int fizz = 3, buzz = 5;
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % fizz != 0 && i % buzz != 0)
+			cout << i ;
+		if (i % fizz == 0)
+			cout << "Fizz";
+		if (i % buzz == 0)
+			cout << "Buzz";
+			
+		cout << endl;
+	}
+
+	system("PAUSE");
+	return 0;
+}
